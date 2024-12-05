ed7fef83b6d6b5ab700526f93f8d54130628e619
Valentín Pico <86754349+Valentinpico@users.noreply.github.com>
2024-12-05 17:23:37 -0500

    creacion de proyecto de veterinaria

---
+++ veterinaria/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className=" bg-gray-700 ">adsfasdfhjk;</h1>
+
+      <button className="p-2 uppercase">adfasd</button>
+    </>
+  );
+}
+
+export default App;
