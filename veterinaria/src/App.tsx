811148a94f1ad53e13e43bd2a19d4ee8fe6054e6
Valentín Pico <86754349+Valentinpico@users.noreply.github.com>
2024-12-05 18:14:12 -0500

    iniciallizar el form

--- veterinaria/src/App.tsx
+++ veterinaria/src/App.tsx
@@ -1,9 +1,27 @@
+import { PatientForm } from "./modules/pacientes/components/Form";
+
 function App() {
   return (
     <>
-      <h1 className=" bg-gray-700 ">adsfasdfhjk;</h1>
+      <div className="mt-10 ">
+        <div className=" flex justify-center">
+          <h1 className="font-black text-2xl uppercase">
+            Agendamiento de pacientes
+          </h1>
+          <span className="ml-2 text-2xl text-indigo-600 uppercase font-black">
+            Veterinaria
+          </span>
+        </div>
 
-      <button className="p-2 uppercase">adfasd</button>
+        <div className="grid w-10/12 mt-11 m-auto bg-slate-100 rounded lg:grid-cols-2 gap-3	">
+          <div className="">
+            <PatientForm />
+          </div>
+          <div className="">
+        {/*     <PatientForm /> */}
+          </div>
+        </div>
+      </div>
     </>
   );
 }
